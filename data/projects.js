328d1ef0c67babfa547652a5bbea49a65228cae7
iAnmol16 <ianmolyadav16@gmail.com>
2026-03-03 23:40:31 +0530

    first

---
+++ data/projects.js
@@ -0,0 +1,10 @@
+const projects = [
+  {
+    title: "AI Resume Analyzer",
+    description: "Analyzes resumes using NLP",
+    tech: ["Python", "Flask", "JS"],
+    image: "assets/images/project1.png",
+    github: "#",
+    live: "#"
+  }
+];
